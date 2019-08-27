8411ed8cf468e6a99d55341b26c103efcdfdbdf0
Akash <akashbaga011@gmail.com>
2019-08-27 16:33:06 +0300

    Binary Search Algorithm

---
+++ binary_search.py
@@ -0,0 +1,26 @@
+def binary_search(arr, lower, upper, x):
+
+    while lower <= upper:
+
+        mid = lower + (upper - lower) // 2
+
+        if arr[mid] == x:
+            return mid
+
+        elif arr[mid] < x:
+            lower = mid + 1
+
+        else:
+            upper = mid - 1
+
+    return -1
+
+
+# Test Case , Expected result = 1
+arr = [1, 2, 3, 4, 5, 6]
+result = binary_search(arr, 0, len(arr) - 1, 1)
+
+if result != -1:
+    print("Element is present at index {}".format(result))
+else:
+    print("Element is not found in array")
